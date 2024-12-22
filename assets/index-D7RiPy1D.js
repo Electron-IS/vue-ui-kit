bc84dae63044ad91f6326d8a06423f5b766caac7
Ilya <65188527+ilyaip@users.noreply.github.com>
2024-12-22 12:00:52 +0300

    Add GitHub Actions workflow for deploying Storybook

---
+++ assets/index-D7RiPy1D.js
@@ -0,0 +1,1 @@
+import{M,u as a}from"./index-x2dlQBfc.js";import"./index-s8_lTpaS.js";import"./iframe-DvghI0q5.js";import"../sb-preview/runtime.js";import"./index-D-8MO0q_.js";import"./index-8LgnguXn.js";import"./index-DrFu-skq.js";export{M as MDXProvider,a as useMDXComponents};
